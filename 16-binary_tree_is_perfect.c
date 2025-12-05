92e282a919080d916d8773f8cb34680547c40de2
LuisJimenez <luisssajs@gmail.com>
2025-12-05 17:44:18 +0000

    task 16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -6,16 +6,12 @@
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-size_t left_height, right_height;
+size_t size, leaves;
 if (tree == NULL)
 return (0);
-left_height = binary_tree_height(tree->left);
-right_height = binary_tree_height(tree->right);
-if (left_height != right_height)
-return (0);
-if (tree->left == NULL && tree->right == NULL)
+size = binary_tree_size(tree);
+leaves = binary_tree_leaves(tree);
+if (size == (2 * leaves - 1))
 return (1);
-if (tree->left != NULL && tree->right != NULL)
-return (binary_tree_is_perfect(tree->left) && binary_tree_is_perfect(tree->right));
 return (0);
 }
